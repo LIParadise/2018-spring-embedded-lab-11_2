d3049b9ffcfbcaaee9bb4d79a66e4cf146fc5e22
LIParadise <a58524andy@gmail.com>
2019-05-16 04:46:07 +0800

    i dunno what happened

--- main.cpp
+++ main.cpp
@@ -6,31 +6,37 @@ LAB EXERCISE - Simple BLE Appcessory
 
 	GOOD LUCK!
  *----------------------------------------------------------------------------*/
- 
+
 #include "mbed.h"
 #include "BLEDevice.h"
 #include "HeartRateService.h"
 #include "BatteryService.h"
 #include "DeviceInformationService.h"
-#include "Utils.h"/* To turn on/off the debug messages on the console edit this file; NEED_CONSOLE_OUTPUT 1/0( it will have an impact on code-size and power consumption.) */
+#include "Utils.h" /* To turn on/off the debug messages on the console edit this file; NEED_CONSOLE_OUTPUT 1/0( it will have an impact on code-size and power consumption.) */
 
-BLEDevice  ble;
+BLEDevice ble;
 DigitalOut led(LED1);
 Ticker update;
 Ticker aliveness;
 
-const static char     DEVICE_NAME[]        = "MY_BLE_HRM";
-static const uint16_t uuid16_list[]        = {GattService::UUID_HEART_RATE_SERVICE,
-                                              GattService::UUID_BATTERY_SERVICE,
-                                              GattService::UUID_DEVICE_INFORMATION_SERVICE};
+const static char DEVICE_NAME[] = "MY_BLE_HRM";
+static const uint16_t uuid16_list[] = {GattService::UUID_HEART_RATE_SERVICE,
+                                       GattService::UUID_BATTERY_SERVICE,
+                                       GattService::UUID_DEVICE_INFORMATION_SERVICE};
 
-static volatile bool  triggerSensorPolling = false;
+static volatile bool triggerSensorPolling = false;
 
 void disconnectionCallback(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
 {
     DEBUG("Disconnected!\n\r");
     DEBUG("Restarting the advertising process\n\r");
     ble.startAdvertising(); // restart advertising
+}
+
+void connectionTimeoutCallback()
+{
+    DEBUG("Connection Timeout\r\n");
+    ble.startAdvertising();
 }
 
 void connectionCallback(Gap::Handle_t handle, const Gap::ConnectionParams_t *reason)
@@ -40,14 +46,14 @@ void connectionCallback(Gap::Handle_t handle, const Gap::ConnectionParams_t *rea
 
 void update_handler(void)
 {
-  /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
+    /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
   *  heavy-weight sensor polling from the main thread. */
     triggerSensorPolling = true;
 }
 
-void aliveness_handler (void)
+void aliveness_handler(void)
 {
-	led = !led;	
+    led = !led;
 }
 
 /*----------------------------------------------------------------------------
@@ -57,35 +63,42 @@ void aliveness_handler (void)
 int main(void)
 {
     update.attach(update_handler, 1);
-	  aliveness.attach(aliveness_handler,0.5);
+    aliveness.attach(aliveness_handler, 0.5);
 
     DEBUG("Initialising \n\r");
     ble.init();
+    ble.setDeviceName((uint8_t*)DEVICE_NAME);
+    ble.setAppearance((uint16_t*)DEVICE_NAME);
     ble.onDisconnection(disconnectionCallback);
     ble.onConnection(connectionCallback);
+    ble.onTimeout(connectionTimeoutCallback);
 
     /* Setup primary service. */
     uint8_t hrmCounter = 100;
     HeartRateService hrService(ble, hrmCounter, HeartRateService::LOCATION_FINGER);
 
     /* Setup auxiliary services. */
-	  uint8_t batterylevel = 25;
-    BatteryService           battery(ble,batterylevel);
-    DeviceInformationService deviceInfo(ble, "ST", "Nucleo", "SN1" );
+    uint8_t batterylevel = 25;
+    BatteryService battery(ble, batterylevel);
+    DeviceInformationService deviceInfo(ble, "ST", "Nucleo", "SN1");
 
     /* Setup advertising. */
     ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
     ble.accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+
     ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
     ble.setAdvertisingInterval(1600); /* 1000ms; in multiples of 0.625ms. */
 
     ble_error_t my_ret_num = ble.startAdvertising();
-    if( my_ret_num != BLE_ERROR_NONE ){
-        DEBUG( "this is in theory possible; error code:%d\n\r", my_ret_num );
-    }else {
-        DEBUG( "the board shall be broken.\n\r");
+    if (my_ret_num != BLE_ERROR_NONE)
+    {
+        DEBUG("this is in theory possible; error code:%d\n\r", my_ret_num);
+    }
+    else
+    {
+        DEBUG("the board shall be broken.\n\r");
     }
 
     while (true)
@@ -97,16 +110,26 @@ int main(void)
             /* Do blocking calls or whatever is necessary for sensor polling. */
             /* In our case, we simply update the dummy HRM measurement. */
             hrmCounter++;
-            
+
             if (hrmCounter == 150)
             {
                 hrmCounter = 80;
             }
 
             hrService.updateHeartRate(hrmCounter);
-        } 
+
+            DEBUG("now hr: %d", hrmCounter);
+        }
         else
         {
+            static int my_counter = 0;
+            my_counter++;
+            wait(1.5);
+            Gap::GapState_t my_gapstate;
+            my_gapstate = ble.getGapState();
+            DEBUG("\r\nconnected:   %d\r\n", my_gapstate.connected);
+            DEBUG("advertising: %d\r\n", my_gapstate.advertising);
+
             ble.waitForEvent();
         }
     }
